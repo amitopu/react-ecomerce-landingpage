130fd158a02a696fc336c3693038355dc08f3767
rupok <nurealamsiddiky@gmail.com>
2022-03-27 22:53:18 +0600

    final commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Header from "./component/Header/Header";
 import Shop from "./component/Shop/Shop";
 import Question1 from "./component/Question1/Question1";
+import Question2 from "./component/Question2/Question2";
 
 function App() {
     return (
@@ -10,6 +11,7 @@ function App() {
             <Header></Header>
             <Shop></Shop>
             <Question1></Question1>
+            <Question2></Question2>
         </div>
     );
 }
--- src/component/Question2/Question2.js
+++ src/component/Question2/Question2.js
@@ -5,10 +5,15 @@ const Question2 = () => {
         <div>
             <h2>How state works in react?</h2>
             <p>
-                State is a boult in object of a react component that holds some
-                state i.e. value. State can be change using this.state and when
-                state of a component is changed re-rendering of that component
-                is occured.
+                State is a object of a react component that holds some state
+                i.e. values that control the behaviour of a component. State can
+                be changed using this.setState and when state of a component is
+                changed re-rendering of that component is occured. Using
+                useState hook state can be manually initiated and an array is
+                returned which contains a function and a variable that holds the
+                state value. The function is used to change the value of the
+                state. This can be done by calling the function with some value
+                as parameter from different function, event handlers etc.
             </p>
         </div>
     );
